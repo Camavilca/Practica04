aa6bbb5d05085733095d03bae02906d7cfc4ec04
Camavilca <ocamavilcachavez@gmail.com>
2018-11-12 22:42:40 -0500

    Se termino consumir el api rest

---
+++ app/src/main/java/camavilca/orlando/practica04/views/MainActivity.java
@@ -0,0 +1,94 @@
+package camavilca.orlando.practica04.views;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+import camavilca.orlando.practica04.R;
+import camavilca.orlando.practica04.adapters.SolicitudAdapter;
+import camavilca.orlando.practica04.models.Solicitud;
+import camavilca.orlando.practica04.services.ApiService;
+import camavilca.orlando.practica04.services.ApiServiceGenerator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private static final int REGISTER_FORM_REQUEST = 100;
+    public RecyclerView listaSolicitudes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listaSolicitudes = findViewById(R.id.lista_solicitudes);
+        listaSolicitudes.setLayoutManager(new LinearLayoutManager(this));
+        listaSolicitudes.setAdapter(new SolicitudAdapter());
+        initialize();
+    }
+    private void initialize() {
+
+        ApiService service = ApiServiceGenerator.createService(ApiService.class);
+
+        Call<List<Solicitud>> call = service.getSolicitudes();
+
+        call.enqueue(new Callback<List<Solicitud>>() {
+            @Override
+            public void onResponse(Call<List<Solicitud>> call, Response<List<Solicitud>> response) {
+                try {
+
+                    int statusCode = response.code();
+                    Log.d(TAG, "HTTP status code: " + statusCode);
+
+                    if (response.isSuccessful()) {
+
+                        List<Solicitud> solicitudes = response.body();
+                        Log.d(TAG, "Solicitudes: " + solicitudes);
+
+                        SolicitudAdapter adapter = (SolicitudAdapter) listaSolicitudes.getAdapter();
+                        adapter.setSolicitudes(solicitudes);
+                        adapter.notifyDataSetChanged();
+
+                    } else {
+                        Log.e(TAG, "onError: " + response.errorBody().string());
+                        throw new Exception("Error en el servicio");
+                    }
+
+                } catch (Throwable t) {
+                    try {
+                        Log.e(TAG, "onThrowable: " + t.toString(), t);
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }catch (Throwable x){}
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Solicitud>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.toString());
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void showRegister(View view){
+        startActivityForResult(new Intent(this, RegisterActivity.class), REGISTER_FORM_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REGISTER_FORM_REQUEST) {
+            // refresh data
+            initialize();
+        }
+    }
+}
